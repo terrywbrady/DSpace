e1bb7072b02f92eddb3167d745528e2d416f3931
Terry Brady <terrywbrady@gmail.com>
2015-12-17 16:28:19 -0800

    x

--- dspace-api/src/main/java/org/dspace/content/Community.java
+++ dspace-api/src/main/java/org/dspace/content/Community.java
@@ -164,15 +164,17 @@ public class Community extends DSpaceObject implements DSpaceObjectLegacySupport
      */
     public List<Community> getParentCommunities()
     {
+    	log.warn("TBTB "+this.getHandle()+" Get");
         return parentCommunities;
     }
 
     void addParentCommunity(Community parentCommunity) {
-    	log.warn("TBTB "+parentCommunity.getHandle());
+    	log.warn("TBTB "+this.getHandle()+" Add:"+parentCommunity);
         //getParentCommunities().add(parentCommunity);
     }
 
     void clearParentCommunities(){
+    	log.warn("TBTB "+this.getHandle()+" Clear");
         this.parentCommunities.clear();
         this.parentCommunities = null;
     }
